1100d33d79761d58495bda608926060e8c34925a
Drew <abemery@clemson.edu>
2021-04-13 02:50:30 -0400

    vulnerabilities everywhere

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import lyricsgenius
 import time
+import sys
+import requests
+import keys
 
-genius = lyricsgenius.Genius()
+# fetch a client token from genius api
+def getToken():
 
+    data = {
+      'client_id': keys.client_id,
+      'client_secret': keys.client_secret,
+      'grant_type': 'client_credentials'
+    }
+
+    response = requests.post('https://api.genius.com/oauth/token', data=data)
+    return response.json()['access_token']
+
+# return artist object
 def getArtist(artistName):
+    genius = lyricsgenius.Genius(getToken())
+    genius.verbose = False
     try:
         artist = genius.search_artist(artistName, max_songs=1, sort="title")
         return artist
     except Exception as e:
         print(e)
 
-
+# return song lyrics as string
 def getSongLyrics(artist, songTitle):
     try:
         song = artist.song(songTitle)
@@ -19,8 +35,9 @@ def getSongLyrics(artist, songTitle):
         print(e)
 
 if __name__ == '__main__':
-    t = time.time()
-    artist = getArtist('the avett brothers')
-    song = getSongLyrics(artist, 'around the world')
+    artistName = sys.argv[1].replace("_", " ")
+    songName = sys.argv[2].replace("_", " ")
+    artist = getArtist(artistName)
+    song = getSongLyrics(artist, songName)
     print(song.lyrics)
-    print('\n\nIt took:\t{} seconds'.format(time.time() - t))
+    sys.stdout.flush()
